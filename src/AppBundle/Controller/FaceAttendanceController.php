e72f8febc09742c606efaeb2f34f772e28da14e9
jichinthaka <imesh.14@cse.mrt.ac.lk>
2017-03-19 10:47:28 +0530

    faceAttendanceSystem

---
+++ src/AppBundle/Controller/FaceAttendanceController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Imesh Chinthaka
+ * Date: 2017-03-19
+ * Time: AM 10.46
+ */
+
+namespace AppBundle\Controller;
+
+
+class FaceAttendanceController
+{
+
+}
