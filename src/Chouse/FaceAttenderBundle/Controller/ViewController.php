e3b1510f7ebdac9843b01ec89d9d627cdf4ff8bd
jichinthaka <imesh.14@cse.mrt.ac.lk>
2017-04-20 14:37:13 +0530

    add atemplet

---
+++ src/Chouse/FaceAttenderBundle/Controller/ViewController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Imesh Chinthaka
+ * Date: 2017-04-20
+ * Time: PM 1.44
+ */
+
+namespace Chouse\FaceAttenderBundle\Controller;
+
+
+class ViewController
+{
+
+}
